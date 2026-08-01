e20ad1a1889e86de954d9f6fa22a24d275b151f3
rajtilak-2020 <rajtilak1062020@gmail.com>
2026-08-01 15:37:14 +0530

    Add Vercel deployment configuration

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Clone Flutter SDK if not present
+if [ ! -d "flutter" ]; then
+  echo "Cloning Flutter SDK..."
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+
+# Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+echo "Building Flutter Web release..."
+flutter build web --release
