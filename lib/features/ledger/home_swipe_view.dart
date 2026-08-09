6f67a2c860860e3da366003c775a2d5c64f5f331
rajtilak-2020 <rajtilak1062020@gmail.com>
2026-08-09 13:12:31 +0530

    refactor(settings): remove unused GitHub SVG path and update cSpell words

--- lib/features/ledger/home_swipe_view.dart
+++ lib/features/ledger/home_swipe_view.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:intl/intl.dart';
 import '../../models/transaction.dart';
